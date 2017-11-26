b40567dbd8d0631d67a329317823cf8163f10374
Oceanos1 <michael.spierer@heig-vd.ch>
2017-11-26 15:18:57 +0100

    add isolation mode

--- src/Main.java
+++ src/Main.java
@@ -25,8 +25,33 @@ public class Main {
                             ";create=true",
                     connectionProps);
         }
-        System.out.println("Connected to database");
+        System.out.println("Connected to database : " + dbName);
         return conn;
+    }
+
+    public static void printUser(Connection conn) throws SQLException {
+
+        Statement s = conn.createStatement();
+        ResultSet r = s.executeQuery("SELECT * FROM transactions.clients;");
+
+
+        System.out.println("Les clients sont: ");
+        while (r.next()) {
+            String nom = r.getString("nom");
+            System.out.println("nom : " + nom);
+        }
+    }
+
+    public static void printIsolationMode(Connection conn) throws SQLException {
+
+        Statement s = conn.createStatement();
+        ResultSet r = s.executeQuery("SELECT @@tx_isolation");
+
+
+        while (r.next()) {
+            System.out.println("Mode d'isolation : "+ r.getString("@@tx_isolation"));
+        }
+
     }
 
     public static void main(String[] args) {
@@ -34,18 +59,8 @@ public class Main {
         try {
 
             Connection con = getConnection(Constantes.USERNAME_ROOT, Constantes.PASSWORD_ROOT, Constantes.DB, Constantes.SERVER_NAME, Constantes.PORT, Constantes.DB_NAME);
-
-            Statement s = con.createStatement();
-            ResultSet r = s.executeQuery("SELECT * FROM transactions.clients;");
-
-
-            System.out.println("Les clients sont: ");
-            while (r.next()) {
-                String nom = r.getString("nom");
-                System.out.println("nom : " + nom);
-            }
+            printIsolationMode(con);
         } catch (Exception e) {
-            // TODO: handle exception
             e.printStackTrace();
 
         }
