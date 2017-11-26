96814a82725be70672f751ce09af5340a8e514c3
Oceanos1 <michael.spierer@heig-vd.ch>
2017-11-26 15:02:54 +0100

    connection etablished

---
+++ src/Constantes.java
@@ -0,0 +1,8 @@
+public interface Constantes {
+    String USERNAME_ROOT = "root";
+    String PASSWORD_ROOT = "";
+    String DB = "mysql";
+    String SERVER_NAME = "localhost";
+    String DB_NAME = "transactions";
+    int PORT = 3306;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,53 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Properties;
+
 
 public class Main {
 
+
+    public static Connection getConnection(String userName, String password, String db, String serverName, int portNumber, String dbName) throws SQLException {
+
+        Connection conn = null;
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", userName);
+        connectionProps.put("password", password);
+
+        if (db.equals("mysql")) {
+            conn = DriverManager.getConnection(
+                    "jdbc:" + db + "://" +
+                            serverName +
+                            ":" + portNumber + "/",
+                    connectionProps);
+        } else if (db.equals("derby")) {
+            conn = DriverManager.getConnection(
+                    "jdbc:" + db + ":" +
+                            dbName +
+                            ";create=true",
+                    connectionProps);
+        }
+        System.out.println("Connected to database");
+        return conn;
+    }
+
     public static void main(String[] args) {
+
         try {
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/transactions","root","");
-            Statement s =  con.createStatement();
+
+            Connection con = getConnection(Constantes.USERNAME_ROOT, Constantes.PASSWORD_ROOT, Constantes.DB, Constantes.SERVER_NAME, Constantes.PORT, Constantes.DB_NAME);
+
+            Statement s = con.createStatement();
             ResultSet r = s.executeQuery("SELECT * FROM transactions.clients;");
 
 
             System.out.println("Les clients sont: ");
-            while(r.next()){
+            while (r.next()) {
                 String nom = r.getString("nom");
-                System.out.println("nom : "+ nom);
+                System.out.println("nom : " + nom);
             }
         } catch (Exception e) {
             // TODO: handle exception
             e.printStackTrace();
 
         }
-    }    }
+    }
+}
